f143e2b15254ef6207635e5775c903f2272d39e7
Praveenkumar-Babu <praveenbabu130731@gmail.com>
2022-07-09 18:28:50 +0530

    Praveen Task Done

---
+++ src/org/trial/GitPractice.java
@@ -0,0 +1,11 @@
+package org.trial;
+
+public class GitPractice {
+private void Test() {
+System.out.println("Test");
+}
+public static void main(String[] args) {
+	GitPractice g = new GitPractice();
+	g.Test();
+}
+}
