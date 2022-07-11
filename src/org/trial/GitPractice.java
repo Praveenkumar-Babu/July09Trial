10043a6e87507dc7b5cf3e5def369c9a66cffdf8
Praveenkumar-Babu <praveenbabu130731@gmail.com>
2022-07-11 16:08:45 +0530

    Pulling codes from master

--- src/org/trial/GitPractice.java
+++ src/org/trial/GitPractice.java
@@ -4,6 +4,11 @@ public class GitPractice {
 private void Test() {
 System.out.println("Test");
 }
+
+private void Test5() {
+System.out.println("Test5");
+}
+
 public static void main(String[] args) {
 	GitPractice g = new GitPractice();
 	g.Test();
