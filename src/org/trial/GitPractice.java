9872fc81ddcef9b9031fbbd109b0f0a09ad70fcb
Praveenkumar-Babu <praveenbabu130731@gmail.com>
2022-07-11 15:58:49 +0530

    Deva modified codes

--- src/org/trial/GitPractice.java
+++ src/org/trial/GitPractice.java
@@ -4,6 +4,15 @@ public class GitPractice {
 private void Test() {
 System.out.println("Test");
 }
+
+private void Test1() {
+System.out.println("Test 1");
+}
+
+private void Test2() {
+System.out.println("Test 2");
+}
+
 public static void main(String[] args) {
 	GitPractice g = new GitPractice();
 	g.Test();
